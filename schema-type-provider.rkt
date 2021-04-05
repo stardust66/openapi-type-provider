ec9b21d72af44d96d7d348166943e2ad7b4fada3
Jason Chen <kbtpodifo@gmail.com>
2021-04-04 21:55:37 -0400

    Add writer generation to schema provider

--- schema-type-provider.rkt
+++ schema-type-provider.rkt
@@ -1,33 +1,41 @@
 #lang typed/racket/base
 
-(provide schema-type-provider)
+(provide schema-type-provider
+         (for-syntax schema->typedef
+                     schema->field-definition)
+         JSExpr
+         string->jsexpr
+         jsexpr->string)
 
 (require (for-syntax racket/base
                      racket/syntax
-                     "json-schema.rkt"))
+                     "json-schema.rkt")
+         typed/json)
 
 (begin-for-syntax
+  (define (upper-name name)
+    (string->symbol (string-titlecase (symbol->string name))))
+
   (define (schema->field-definition schema)
     (define name (Schema-name schema))
-    (define typename (schema->type schema))
+    (define typename (schema->typename schema))
     `[,name : ,typename])
 
-  (define (schema->type schema)
+  (define (schema->typename schema)
     (cond
       [(Schema-String? schema) `String]
       [(Schema-Integer? schema) `Integer]
       [(Schema-Number? schema) `Number]
-      [(Schema-Object? schema) (Schema-name schema)]))
+      [(Schema-Object? schema) (upper-name (Schema-name schema))]))
 
   (define ((schema->hashref hashtable) schema)
     (define name (Schema-name schema))
-    (define typename (schema->type schema))
+    (define typename (schema->typename schema))
     `(cast (hash-ref ,hashtable (quote ,name)) ,typename))
 
   (define (schema->read schema)
     (define name (Schema-name schema))
     (define read-name (format-symbol "read-~a" name))
-    (define property-names (map Schema-name (Schema-Object-properties schema)))
     (define properties (Schema-Object-properties schema))
     `(begin
        (: ,read-name (-> String ,name))
@@ -35,11 +43,38 @@
          (define contents (cast (string->jsexpr jsonstr) (HashTable Symbol JSExpr)))
          (,name ,@(map (schema->hashref 'contents) properties)))))
 
-  (define (schema->struct schema)
+  (define (schema->writer schema)
+    (cond
+      [(Schema-String? schema) 'values]
+      [(Schema-Number? schema) 'number->string]
+      [(Schema-Integer? schema) 'number->string]
+      [(Schema-Object? schema) (format-symbol "write-~a" (Schema-name schema))]))
+
+  (define (schema->jsexpr schema)
+    (define properties (Schema-Object-properties schema))
     (define name (Schema-name schema))
-    (define field-defs
-      (map schema->field-definition (Schema-Object-properties schema)))
-    `(struct ,name (,@field-defs) #:transparent)))
+    (define write-name (format-symbol "write-~a" name))
+    `(begin
+       (: ,write-name (-> ,name String))
+       (define (,write-name obj)
+         (jsexpr->string
+          (make-hasheq
+           (list
+            ,@(map
+               (λ (property)
+                 (define property-name (Schema-name property))
+                 (define getter (format-symbol "~a-~a" name property-name))
+                 `(cons (quote ,property-name)
+                        (,(schema->writer property) (,getter obj))))
+               properties)))))))
+
+  (define (schema->typedef schema)
+    (define name (upper-name (Schema-name schema)))
+    (cond
+      [(Schema-Object? schema)
+       (define field-defs
+         (map schema->field-definition (Schema-Object-properties schema)))
+       `(struct ,name (,@field-defs) #:transparent)])))
 
 (define-syntax (schema-type-provider stx)
   (syntax-case stx ()
@@ -48,7 +83,7 @@
        (define schema (file->json-schema (syntax-e #'filename)))
        (define definitions
          `(begin
-            ,(schema->struct schema)
-            ,(schema->read schema)))
-       (displayln definitions)
+            ,(schema->typedef schema)
+            ,(schema->read schema)
+            ,(schema->jsexpr schema)))
        (datum->syntax stx definitions stx stx))]))
