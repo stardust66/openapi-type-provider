f8164c50d3d1b6c91e0da9e5bcac8e1b3d5b3a7a
Jason Chen <kbtpodifo@gmail.com>
2021-04-11 19:48:01 -0400

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,15 @@
+#lang info
+
+(define version "0.1")
+
+(define collection "openapi-type-provider")
+
+(define deps '("base"
+               "typed-racket-lib"
+               "typed-racket-more"))
+
+(define pkg-desc "Generate well typed client library from OpenAPI schema.")
+
+(define pkg-authors '("kbtpodifo@gmail.com"))
+
+(define scribblings '(("scribblings/openapi-type-provider.scrbl")))
