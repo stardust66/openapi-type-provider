a122272b216251586129e2130c798e4f4ba5d68e
Jason Chen <kbtpodifo@gmail.com>
2021-04-23 16:08:29 -0400

    Add main.rkt for providing definitions

---
+++ main.rkt
@@ -0,0 +1,7 @@
+#lang typed/racket/base
+
+(provide (all-from-out "openapi-type-provider.rkt"
+                       "schema-type-provider.rkt"))
+
+(require "openapi-type-provider.rkt"
+         "schema-type-provider.rkt")
